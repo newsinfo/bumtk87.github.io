1314cc39d2b7835df0e5cbe40029ff7c0c58c476
김준호 <14news@naver.com>
2017-11-08 11:05:36 +0900

    1

---
+++ default/00/01.php
@@ -0,0 +1,127 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr" />
+<meta http-equiv="imagetoolbar" content="no">
+<title>세력테마연구소</title>
+<meta name="description" content="후불제 주식/수익부터 내드리는 양심적인업체/ 급등주/세력주 발굴">
+<meta name="viewport" content="width=device-width">
+<meta name="naver-site-verification" content="naver7d8f8ec3d7980f6add5e9c9281080c2b.html"/> 
+<meta name="naver-site-verification" content="a6f0ba60e8c884183590b3b2790a0215e3182360"/>
+
+<meta property="og:type" content="website">
+<meta property="og:title" content="세력테마연구소">
+<meta property="og:description" content="세력테마연구소 후불제 주식,수익부터 내드리는 양심적인업체, 급등주,세력주 발굴">
+<meta property="og:image" content="http://www.stockplay.co.kr/default/img/vdata/images/v_00.jpg">
+<link rel="canonical" href="http://www.stockplay.co.kr/default/00/01.php">
+<meta name="twitter:card" content="stock">
+<meta name="twitter:title" content="세력테마연구소">
+<meta name="twitter:description" content="후불제 주식,수익부터 내드리는 양심적인업체, 급등주,세력주 발굴">
+<meta name="twitter:image" content="http://www.stockplay.co.kr/default/img/vdata/images/v_00.jpg">
+<meta name="twitter:domain" content="http://www.stockplay.co.kr">
+
+<meta property="al:ios:app_store_id" content="">
+<meta property="al:ios:app_name" content="">
+
+<meta property="al:android:app_name" content="App Links">
+<meta property="al:android:package" content="org.applinks">
+
+<link rel="stylesheet" href="../img/vdata/inc/cs.css" type="text/css">
+<script type='text/javascript' src='http://code.jquery.com/jquery-1.8.0.min.js'></script>
+<script type='text/javascript'>
+//<![CDATA[
+var cnt = 0;
+var pos = 0;
+var ary = [
+['../img/vdata/images/v_00.jpg','../img/vdata/images/v_00.jpg'],
+];
+$(document).ready(function()
+{
+setImg();
+});
+function setImg()
+{
+fncChgImg(ary[pos][1]);
+setTimeout(setImg,5000);
+pos++;
+if(pos>=ary.length) pos=0;
+}
+function fncChgImg(imgSrc)
+{
+cnt++;
+if((cnt%2)==0)
+{
+$('#ig0').fadeOut(2000);
+$('#ig1').attr('src',imgSrc).fadeIn(2000);
+}
+else
+{
+$('#ig1').fadeOut(2000);
+$('#ig0').attr('src',imgSrc).fadeIn(2000);
+}
+}
+function fncShow()
+{
+$('#ig').attr('src',cimg).fadeIn('slow');
+}
+function bluring(){ 
+if(event.srcElement.tagName=="A"||event.srcElement.tagName=="IMG") document.body.focus(); 
+} 
+document.onfocusin=bluring;
+//]]>
+</script>
+</head>
+<body>
+{{$menu}}
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td height="450" align="center" valign="top" style="background:url(../img/vdata/images/bg_03.jpg) repeat-x;"><table width="1000" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td align="left" valign="top"><form name='frm' style="text-align:left ">
+<img id='ig0' src='../img/vdata/images/v_00.jpg' style='position:absolute;' />
+<img id='ig1' src='../img/vdata/images/v_00.jpg' style='position:absolute;' />
+</form></td>
+</tr>
+</table></td>
+</tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td align="center" valign="top" background="../img/vdata/images/bg_03.jpg"><table width="1000" border="0" cellspacing="0" cellpadding="0">
+<tr align="left" valign="top">
+<td width="330" height="150"><table width="275" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td align="left" valign="top"><a href="../05/01.php"><img src="../img/vdata/images/tt_news.jpg" border="0"></a></td>
+</tr>
+<tr>
+<td align="left" valign="top" style="padding-left:5px; padding-top:10px ">{{$cafe_board_output_3}}</td>
+</tr>
+</table></td>
+<td width="468"><table width="420" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td align="left" valign="top"><a href="../03/01.php"><img src="../img/vdata/images/tt_gallery.jpg" border="0"></a></td>
+</tr>
+<tr>
+<td align="left" valign="top" style="padding-left:5px; padding-top:6px ">{{$cafe_board_output_12}}</td>
+</tr>
+</table></td>
+<td width="202"><img src="../img/vdata/images/cs_main.jpg"></td>
+</tr>
+</table>
+<table width="1000" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td height="35" align="left" valign="top"><img src="../img/vdata/images/line.jpg" width="1000" height="1"></td>
+</tr>
+</table>
+<table width="1000" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td height="140" align="center" valign="top"><img src="../img/vdata/images/banner.jpg" border="0" usemap="#bannermain">
+<map name="bannermain" id="bannermain">
+<area shape="rect" coords="685,4,995,99" href="http://cafe.naver.com/stst123" target="_blank" onFocus="this.blur()";>
+<area shape="rect" coords="336,4,685,99" href="http://cafe.naver.com/stst123" target="_blank" onFocus="this.blur()";>
+<area shape="rect" coords="2,2,312,97" href="../01/01.php" target="_self" onFocus="this.blur()";>
+</map></td>
+</tr>
+</table></td>
+</tr>
+</table>
+{{$copy}}
